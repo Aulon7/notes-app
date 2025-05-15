0dff282a5cf12285b9c05100f715bd98a0b9b0fd
Your Name <you@example.com>
2025-05-15 21:15:16 +0200

    vite config fix

--- notes-app-frontend/vite.config.ts
+++ notes-app-frontend/vite.config.ts
@@ -1,21 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  theme: {
-    extend: {
-      animation: {
-        fadeIn: "fadeIn 0.5s ease-in forwards",
-      },
-      keyframes: {
-        fadeIn: {
-          "0%": { opacity: "0", transform: "scale(0.95)" },
-          "100%": { opacity: "1", transform: "scale(1)" },
-        },
-      },
-    },
-  },
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 });
